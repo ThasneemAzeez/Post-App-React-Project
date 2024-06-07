30573140598880a60882606eb335e841b5f94d03
ThasneemAzeez <thasneemazeez2001@gmail.com>
2024-06-07 21:50:43 +0530

    user completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import ForntPage from './component/ForntPage';
 import FeedPage from './component/FeedPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SearchUser from './component/SearchUser';
+import User from './component/User';
 
 function App() {
   return (
     <div >
      <ForntPage/>
      <FeedPage/>
+     <SearchUser/>
+     <User/>
     </div>
   );
 }
