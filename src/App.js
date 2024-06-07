c8034d262fdb6249b5000a3ed2689ede64b4ebbf
ThasneemAzeez <thasneemazeez2001@gmail.com>
2024-06-07 21:18:13 +0530

    feedpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import ForntPage from './component/ForntPage';
+import FeedPage from './component/FeedPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div >
-      <ForntPage/>
+     <ForntPage/>
+     <FeedPage/>
     </div>
   );
 }
---
+++ src/component/FeedPage.jsx
@@ -0,0 +1,101 @@
+import React from 'react'
+
+const FeedPage = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+              <h1>Stories---</h1>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+
+                <div class="card">
+                  <img src="https://img.freepik.com/premium-vector/instagram-story-icon-instagram-stories-live-stream-new-story-hashtag-vector-set-instagram-stories-badges-circle-instagram-stories-frames-editorial-illustration-rivne-ukraine-january-9-2022_564974-247.jpg" height="250px" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+
+                  </div>
+                </div>
+
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card">
+                  <img src="https://as2.ftcdn.net/v2/jpg/05/82/75/09/1000_F_582750940_0wLAueijRLzQtcVg7hNJ9lON11QegfTb.jpg" height="250px" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+
+                  </div>
+                </div>
+
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card">
+                  <img src="https://as2.ftcdn.net/v2/jpg/05/82/75/09/1000_F_582750940_0wLAueijRLzQtcVg7hNJ9lON11QegfTb.jpg" height="250px" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+
+                  </div>
+                </div>
+
+
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card">
+                  <img src="https://as2.ftcdn.net/v2/jpg/05/82/75/09/1000_F_582750940_0wLAueijRLzQtcVg7hNJ9lON11QegfTb.jpg" height="250px" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+            <div className="row g-3">
+              <h1>your feed</h1>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                <div class="card" >
+                  <img src="https://www.tsemrinpoche.com/wp-content/uploads/2012/01/pic02.jpg" class="card-img-top" alt="..."></img>
+                    <div class="card-body">
+                      
+                    </div>
+                    <h6>share</h6>
+                </div>
+
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+              <div class="card" >
+                  <img src="https://wallpapers.com/images/hd/interesting-pictures-0btcclivllsacsw2.jpg" class="card-img-top" alt="..."></img>
+                    <div class="card-body">
+                      
+                    </div>
+                    <h6>share</h6>
+                </div>
+
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+              <div class="card" >
+                  <img src="https://www.allearsenglish.com/wp-content/uploads/2014/09/AEE-185-Are-You-Interested-or-Interesting-Find-Out-the-Difference-in-English.jpg" class="card-img-top" alt="..."></img>
+                    <div class="card-body">
+                      
+                    </div>
+                    <h6>share</h6>
+                </div>
+
+
+
+              </div>
+              <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FeedPage
